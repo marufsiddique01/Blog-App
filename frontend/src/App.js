afd7da5977cea1d714e0372854548eadbf701481
marufsiddique01 <maruf.siddique@northsouth.edu>
2022-08-06 10:40:50 +0600

    app routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Auth from './Components/Auth'
 import Header from './Components/Header'
+import Blogs from './Components/Blogs'
+import UserBlogs from './Components/UserBlogs'
+import BlogDetails from './Components/BlogDetails'
+import AddBlog from './Components/AddBlog'
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-    </div>
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path='/auth' element={<Auth />} />
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='/myBlogs' element={<UserBlogs />} />
+          <Route path='/myBlogs/:id' element={<BlogDetails />} />
+          <Route path='/blogs/add' element={<AddBlog />} />
+        </Routes>
+      </main>
+    </React.Fragment>
+    // <div className='App'>
+    //   {/* <Header /> */}
+    // </div>
   )
 }
 
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -4,16 +4,16 @@ import { borderRadius } from '@mui/system'
 
 const Header = () => {
   return (
-    <AppBar sx={{ background: '#fff' }}>
+    <AppBar sx={{ background: '#fff' }} position='sticky'>
       <Toolbar>
         <Typography variant='h4' sx={{ color: 'teal' }}>
           Blogs App
         </Typography>
-        <Box display='flex' marginLeft='auto'>
+        <Box display='flex' marginLeft='auto' gap='10px'>
           <Button sx={{ margin: '1' }} variant='contained' color='primary'>
             Login
           </Button>
-          <Button sx={{ margin: '1', borderRadius: '10px' }} variant='outlined' color='primary'>
+          <Button variant='contained' color='primary'>
             SignUp
           </Button>
         </Box>
